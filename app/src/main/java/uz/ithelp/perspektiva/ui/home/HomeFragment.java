0dd1480a831aabf5629997a88f560a6293b6542d
shoxumarzoda <ujBP3wXYUstD23d>
2022-05-25 12:29:33 +0500

    yuklash

---
+++ app/src/main/java/uz/ithelp/perspektiva/ui/home/HomeFragment.java
@@ -0,0 +1,41 @@
+package uz.ithelp.perspektiva.ui.home;
+
+import static android.os.Build.VERSION_CODES.R;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+
+import uz.ithelp.perspektiva.databinding.FragmentHomeBinding;
+
+public class HomeFragment extends Fragment {
+
+    private FragmentHomeBinding binding;
+    ConstraintLayout constraintLayout;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        HomeViewModel homeViewModel =
+                new ViewModelProvider(this).get(HomeViewModel.class);
+
+        binding = FragmentHomeBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+
+
+
+        return root;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+}
