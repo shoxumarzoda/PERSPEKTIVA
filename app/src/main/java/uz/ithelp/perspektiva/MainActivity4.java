3ef427447293d93e4639a996151fbefa53c40936
shoxumarzoda <ujBP3wXYUstD23d>
2022-06-28 11:00:43 +0500

    activity4 yaratildi

---
+++ app/src/main/java/uz/ithelp/perspektiva/MainActivity4.java
@@ -0,0 +1,14 @@
+package uz.ithelp.perspektiva;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity4 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main4);
+    }
+}
--- app/src/main/java/uz/ithelp/perspektiva/ui/gallery/GalleryFragment.java
+++ app/src/main/java/uz/ithelp/perspektiva/ui/gallery/GalleryFragment.java
@@ -1,5 +1,6 @@
 package uz.ithelp.perspektiva.ui.gallery;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -18,6 +19,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 
+import uz.ithelp.perspektiva.MainActivity4;
 import uz.ithelp.perspektiva.R;
 import uz.ithelp.perspektiva.databinding.FragmentGalleryBinding;
 
@@ -51,14 +53,14 @@ public class GalleryFragment extends Fragment implements UserAdapter.SelectedUse
 
     private void initial() {
 
-        modelRcyclers.add(new ModelRcycler(R.drawable.author,"1-video. Perspektiv tasvir qurish usullari","Perspektiv tasvir qurish usullari borasida to'liq...",""));
-        modelRcyclers.add(new ModelRcycler(R.drawable.author,"2-video. Eng yaxshi ko'rish burchagi","Eng yaxshi ko'rish burchagi mavzusida qiziqarli faktlar...",""));
-        modelRcyclers.add(new ModelRcycler(R.drawable.author,"3-video. Perspektiv masshtablar 1-qism","Perspektiv masshtablar bilan ishlash borasida...",""));
-        modelRcyclers.add(new ModelRcycler(R.drawable.author,"4-video. Perspektiv masshtablar 2-qism","Perspektiv masshtablar bilan ishlash borasida...",""));
-        modelRcyclers.add(new ModelRcycler(R.drawable.author,"5-video. Qopqog'i ochilgan quti perspektivasi","Qopqog'i ochilgan quti perspektivasi ...",""));
-        modelRcyclers.add(new ModelRcycler(R.drawable.author,"6-video. Arxitektorlar usuli","Loyihalash amaliyotda o‘zining yasalishi jixatidan soddaligi sababli keng tarqalgan perspektiva...",""));
-        modelRcyclers.add(new ModelRcycler(R.drawable.author,"7-video. Radial nurlar izi usuli","Radial nurlar izi usuli ...",""));
-        modelRcyclers.add(new ModelRcycler(R.drawable.author,"8-video. To'rlar usuli","Mikrorayon,  xiyobonlar,  sport  komplekslari  va  shu  kabi  katta  maydonlarga  ega  bo'lgan  inshootlarning  perspektivasini  yasashda  to 'r  usuli  eng  qulay  usul hisoblanadi...",""));
+        modelRcyclers.add(new ModelRcycler(R.drawable.video,"1-video. Perspektiv tasvir qurish usullari","Perspektiv tasvir qurish usullari borasida to'liq...",""));
+        modelRcyclers.add(new ModelRcycler(R.drawable.video,"2-video. Eng yaxshi ko'rish burchagi","Eng yaxshi ko'rish burchagi mavzusida qiziqarli faktlar...",""));
+        modelRcyclers.add(new ModelRcycler(R.drawable.video,"3-video. Perspektiv masshtablar 1-qism","Perspektiv masshtablar bilan ishlash borasida...",""));
+        modelRcyclers.add(new ModelRcycler(R.drawable.video,"4-video. Perspektiv masshtablar 2-qism","Perspektiv masshtablar bilan ishlash borasida...",""));
+        modelRcyclers.add(new ModelRcycler(R.drawable.video,"5-video. Qopqog'i ochilgan quti perspektivasi","Qopqog'i ochilgan quti perspektivasi ...",""));
+        modelRcyclers.add(new ModelRcycler(R.drawable.video,"6-video. Arxitektorlar usuli","Loyihalash amaliyotda o‘zining yasalishi jixatidan soddaligi sababli keng tarqalgan perspektiva...",""));
+        modelRcyclers.add(new ModelRcycler(R.drawable.video,"7-video. Radial nurlar izi usuli","Radial nurlar izi usuli ...",""));
+        modelRcyclers.add(new ModelRcycler(R.drawable.video,"8-video. To'rlar usuli","Mikrorayon,  xiyobonlar,  sport  komplekslari  va  shu  kabi  katta  maydonlarga  ega  bo'lgan  inshootlarning  perspektivasini  yasashda  to 'r  usuli  eng  qulay  usul hisoblanadi...",""));
 
     }
 
@@ -70,6 +72,8 @@ public class GalleryFragment extends Fragment implements UserAdapter.SelectedUse
 
     @Override
     public void selectedUser(ModelRcycler modelRcycler) {
-
+        Intent intent = new Intent(getContext(), MainActivity4.class);
+        intent.putExtra("data", modelRcycler.getLoadUrl());
+        startActivity(intent);
     }
 }
